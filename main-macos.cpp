79f43c2bb943423366a477569d48d5292057627b
UIT 25730039 <25730039@ms.uit.edu.vn>
2025-12-19 13:32:59 +0700

    sync: cập nhật main-macos.cpp với các tính năng từ main.cpp

--- main-macos.cpp
+++ main-macos.cpp
@@ -10,7 +10,18 @@
 using namespace std;
 #define H 20
 #define W 15
+#define MIN_DELAY 100
+#define BASE_DELAY 1000
+#define SPEED_STEP 40
+#define LINES_PER_LEVEL 5
+#define CELL_EMPTY "  "
+#define CELL_BLOCK "[]"
+
 char board[H][W] = {};
+
+int level = 1;
+int totalLines = 0;
+int fallDelay = BASE_DELAY;
 
 // Cross-platform replacements for Windows conio.h functions
 bool kbhit() {
@@ -142,26 +153,100 @@ void boardDelBlock() {
 void initBoard() {
   for (int i = 0; i < H; i++)
     for (int j = 0; j < W; j++)
-      if (i == 0 || i == H - 1 || j == 0 || j == W - 1)
-        board[i][j] = '#';
-      else
-        board[i][j] = ' ';
+      board[i][j] = ' ';
 }
 
 void draw() {
   system("clear");
 
-  for (int i = 0; i < H; i++, cout << endl)
-    for (int j = 0; j < W; j++)
-      cout << board[i][j];
+  const int playW = W - 2;
+  const int playH = H - 1;
+
+  // Top border
+  cout << "+" << string(playW * 2 + 2, '-') << "+\n";
+
+  for (int i = 0; i < playH; i++) {
+    cout << "|+";
+    for (int j = 1; j <= W - 2; j++) {
+      if (board[i][j] == ' ')
+        cout << CELL_EMPTY;
+      else
+        cout << CELL_BLOCK;
+    }
+    cout << "+|\n";
+  }
+
+  // Bottom border
+  cout << "+" << string(playW * 2 + 2, '-') << "+\n";
+
+  cout << "Level: " << level
+       << " | Lines: " << totalLines
+       << " | Delay: " << fallDelay << "ms\n";
 }
 
-void removeLine() {
-  // TODO: Implement line removal logic
+/**
+ * Xóa line
+ * @return số line đã xóa
+ */
+int removeLine() {
+    int removed = 0;
+
+    for (int i = H - 2; i > 0; i--) {
+        bool full = true;
+
+        for (int j = 1; j < W - 1; j++) {
+            if (board[i][j] == ' ') {
+                full = false;
+                break;
+            }
+        }
+
+        if (full) {
+            removed++;
+
+            for (int r = i; r > 1; r--) {
+                for (int c = 1; c < W - 1; c++) {
+                    board[r][c] = board[r - 1][c];
+                }
+            }
+
+            for (int c = 1; c < W - 1; c++) {
+                board[1][c] = ' ';
+            }
+
+            i++;
+        }
+    }
+
+    return removed;
+}
+
+/**
+ * Cập nhật tốc độ block rơi sau khi xóa line
+ * @param linesRemoved số line đã xóa
+ */
+void updateSpeed(int linesRemoved) {
+    // Cộng dồn tổng số dòng đã xóa từ đầu game
+    totalLines += linesRemoved;
+
+    // Tính level hiện tại
+    // Ví dụ:
+    // 0–4 dòng  -> level 1
+    // 5–9 dòng  -> level 2
+    // 10–14 dòng -> level 3
+    level = totalLines / LINES_PER_LEVEL + 1;
+
+    // Giảm delay để tăng tốc
+    fallDelay = BASE_DELAY - (level - 1) * SPEED_STEP;
+
+    // Giới hạn tốc độ tối đa
+    if (fallDelay < MIN_DELAY) {
+        fallDelay = MIN_DELAY;
+    }
 }
 
 int main() {
-  srand(time(nullptr));
+  srand(time(0));
   x = 5;
   y = 0;
   b = rand() % 7;
@@ -174,7 +259,7 @@ int main() {
         x--;
       if (c == 'd' && canMove(1, 0))
         x++;
-      if (c == 's' && canMove(0, 1))
+      if (c == 'x' && canMove(0, 1))
         y++;
       if (c == 'q')
         break;
@@ -183,14 +268,15 @@ int main() {
       y++;
     else {
       block2Board();
-      removeLine();
+      const int linesRemoved = removeLine();
+      updateSpeed(linesRemoved);
       x = 5;
       y = 0;
       b = rand() % 7;
     }
     block2Board();
     draw();
-    this_thread::sleep_for(chrono::milliseconds(500));
+    this_thread::sleep_for(chrono::milliseconds(fallDelay));
   }
   return 0;
 }
