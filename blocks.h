69e988d62af8e0f164f6ce2017bf6fc040024800
UIT 25730039 <25730039@ms.uit.edu.vn>
2025-12-21 13:06:24 +0700

    Add IBlock class with rotation functionality to blocks.h

--- blocks.h
+++ blocks.h
@@ -30,9 +30,48 @@ public:
     }
 };
 
+// I-Block: có 2 trạng thái xoay
+class IBlock : public Blocks {
+public:
+    IBlock() {
+        shape[0][1] = 'I';
+        shape[1][1] = 'I';
+        shape[2][1] = 'I';
+        shape[3][1] = 'I';
+        rotationState = 0;
+    }
+    
+    void rotate() override {
+        // Xóa shape cũ
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                shape[i][j] = ' ';
+        
+        if (rotationState == 0) {
+            // Chuyển từ dọc sang ngang
+            shape[1][0] = 'I';
+            shape[1][1] = 'I';
+            shape[1][2] = 'I';
+            shape[1][3] = 'I';
+            rotationState = 1;
+        } else {
+            // Chuyển từ ngang sang dọc
+            shape[0][1] = 'I';
+            shape[1][1] = 'I';
+            shape[2][1] = 'I';
+            shape[3][1] = 'I';
+            rotationState = 0;
+        }
+    }
+};
+
 // Hàm factory để tạo block mới dựa trên type
 inline Blocks* createBlock(int type) {
-    
+    switch (type) {
+        case 0: return new IBlock();
+       
+        default: return new IBlock();
+    }
 }
 
 #endif // BLOCKS_H
